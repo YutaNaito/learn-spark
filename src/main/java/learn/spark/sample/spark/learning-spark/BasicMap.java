fdc329ffda22bd1f1c22d6887f9137e5ce009bd6
Yuta Naito <44762745+YutaNaito@users.noreply.github.com>
2023-10-24 21:57:28 +0900

    add BasicMap.java

---
+++ src/main/java/learn/spark/sample/spark/learning-spark/BasicMap.java
@@ -0,0 +1,27 @@
+/**
+ * Illustrates a simple map in Java
+ */
+package learn.spark.sample.spark;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.sql.SparkSession;
+
+public class BasicMap {
+  public static void main(String[] args) throws Exception {
+    SparkConf sparkConf = new SparkConf().setAppName("BasicMap");
+    JavaSparkContext sc = new JavaSparkContext(sparkConf);
+    SparkSession sparkSession = SparkSession.builder().config(sparkConf).getOrCreate();
+    JavaRDD<Integer> rdd = sc.parallelize(Arrays.asList(1, 2, 3, 4));
+    JavaRDD<Integer> result = rdd.map(
+      new Function<Integer, Integer>() { public Integer call(Integer x) { return x*x;}});
+    System.out.println(StringUtils.join(result.collect(), ","));
+  }
+}
